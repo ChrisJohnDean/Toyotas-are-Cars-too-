e3d47ec534b0fbf7cbfd49469caa9f616eef2180
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-01 00:26:04 -0800

    Initial Commit

---
+++ Toyotas are Cars too!/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyotas are Cars too!
+//
+//  Created by Chris Dean on 2018-02-01.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
